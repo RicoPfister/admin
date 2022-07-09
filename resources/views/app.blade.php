35a308e3f8a9202ff8af9f4b587e5779c3879bc6
Rico Pfister <contact.rico.pfister@gmail.com>
2022-07-09 18:43:50 +0200

    Change Vite configuration

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,7 @@
         @routes
         @vite('resources/js/app.js')
         @inertiaHead
+        @vite('resources/js/app.js')
     </head>
     <body class="font-sans antialiased">
         @inertia

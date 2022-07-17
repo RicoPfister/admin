21543a2bd2f66665524e2ee5392bbf7129486215
Rico Pfister <contact.rico.pfister@gmail.com>
2022-07-17 18:46:55 +0200

    Add Component Nav and List

---
+++ resources/js/Components/Nav.vue
@@ -0,0 +1,32 @@
+<template>
+
+<div class="border border-gray-500 bg-gray-300 p-3 pl-4 rounded">
+    <div class="flex justify-between">
+        <div><b>Rico Admin</b></div>
+        <div class="flex">
+            <div class="text-stone-600">Accounting</div>
+            <div>&nbsp;|&nbsp;</div>
+            <div class="text-rose-800">Contacts</div>
+            <div>&nbsp;|&nbsp;</div>
+            <div class="text-rose-600">Facts</div>
+            <div>&nbsp;|&nbsp;</div>
+            <div class="text-amber-600">Journal</div>
+            <div>&nbsp;&lt;&nbsp;&gt;&nbsp;</div>
+            <div class="text-blue-500">Psychology</div>
+            <div>&nbsp;|&nbsp;</div>
+            <div class="text-blue-700">Medicine</div>
+            <div>&nbsp;|&nbsp;</div>
+            <div class="text-purple-600">Engineering</div>
+            <div>&nbsp;&lt;&nbsp;&gt;&nbsp;</div>
+            <div class="text-green-800">Filter</div>
+            <div>&nbsp;&lt;&nbsp;&gt;&nbsp;</div>
+            <div class="text-green-600">Statistic</div>
+            <div>&nbsp;&lt;&nbsp;&gt;&nbsp;</div>
+            <div class="text-pink-500">Ricomazier</div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script setup></script>

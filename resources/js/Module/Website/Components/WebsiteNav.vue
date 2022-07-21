a4b2bce8ae4d9d54fdfc8b7342ecd7c93bd5c676
Rico Pfister <contact.rico.pfister@gmail.com>
2022-07-21 09:40:33 +0200

    Change MainNav behavior to single open/close

--- resources/js/Module/Website/Components/WebsiteNav.vue
+++ resources/js/Module/Website/Components/WebsiteNav.vue
@@ -1,14 +1,49 @@
 <template>
-<div :class="top" class="w-full flex justify-center fixed gap-2 duration-500">
-    <div class="grid top-40" @mouseover='OverMainNav1' @mouseout='OutMainNav1'>
-        <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Curriculum<br>Hobbies<br>Social Engagements</div>
-        <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">About Me</div>
+<div class="fixed flex justify-center w-full gap-2">
+
+    <div :class="top" class="relative gap-2 duration-500 border-black">
+        <div class="" @mouseover='OverMainNav' @mouseout='OutMainNav'>
+            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Curriculum<br>Hobbies<br>Social Engagements</div>
+            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">About Me</div>
+        </div>
     </div>
+
+    <div :class="top2" class="relative gap-2 duration-500 border-black">
+        <div class="" @mouseover='OverMainNav2' @mouseout='OutMainNav2'>
+            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Curriculum2<br>Hobbies<br>Social Engagements</div>
+            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">About Me</div>
+        </div>
+    </div>
+
+    <div :class="top3" class="relative gap-2 duration-500 border-black">
+        <div class="" @mouseover='OverMainNav3' @mouseout='OutMainNav3'>
+            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Curriculum2<br>Hobbies<br>Social Engagements</div>
+            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">About Me</div>
+        </div>
+    </div>
+
+    <div :class="top4" class="relative gap-2 duration-500 border-black">
+        <div class="" @mouseover='OverMainNav4' @mouseout='OutMainNav4'>
+            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Curriculum2<br>Hobbies<br>Social Engagements</div>
+            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">About Me</div>
+        </div>
+    </div>
+
+    <div :class="top5" class="relative gap-2 duration-500 border-black">
+        <div class="" @mouseover='OverMainNav5' @mouseout='OutMainNav5'>
+            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Curriculum2<br>Hobbies<br>Social Engagements</div>
+            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">About Me</div>
+        </div>
+    </div>
+</div>
+
+<!-- <div :class="top" class="w-full flex justify-center fixed gap-2 duration-500">
     <div class="">
         <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3"></div>
-        <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">Website/Blog</div>
+        <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">Rico AI</div>
     </div>
-    <div class="">
+</div> -->
+    <!-- <div class="">
         <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3"></div>
         <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">Rico AI</div>
     </div>
@@ -19,8 +54,8 @@
     <div class="">
         <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3"></div>
         <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">Contact</div>
-    </div>
-</div>
+    </div> -->
+
 </template>
 
 <script setup>
@@ -36,8 +71,12 @@ let sideMenu = document.getElementById('main-menu');
 // of the event name when doing it this way.
 
 let top = ref('top-0');
+let top2 = ref('top-0');
+let top3 = ref('top-0');
+let top4 = ref('top-0');
+let top5 = ref('top-0');
 
-function OverMainNav1(){
+function OverMainNav(){
 top.value = 'top-0';
 //        top = '-top-36';
 //    console.log(top);
@@ -47,8 +86,78 @@ top.value = 'top-0';
         // sideMenu.classList.remove('top-[-250px]');
         // sideMenu.classList.add('top-0');+
 };
-function OutMainNav1(){
+
+function OverMainNav2(){
+top2.value = 'top-0';
+//        top = '-top-36';
+//    console.log(top);
+//    alert(top);
+
+
+        // sideMenu.classList.remove('top-[-250px]');
+        // sideMenu.classList.add('top-0');+
+};
+
+function OverMainNav3(){
+top3.value = 'top-0';
+//        top = '-top-36';
+//    console.log(top);
+//    alert(top);
+
+
+        // sideMenu.classList.remove('top-[-250px]');
+        // sideMenu.classList.add('top-0');+
+};
+
+function OverMainNav4(){
+top4.value = 'top-0';
+//        top = '-top-36';
+//    console.log(top);
+//    alert(top);
+
+
+        // sideMenu.classList.remove('top-[-250px]');
+        // sideMenu.classList.add('top-0');+
+};
+
+function OverMainNav5(){
+top5.value = 'top-0';
+//        top = '-top-36';
+//    console.log(top);
+//    alert(top);
+
+
+        // sideMenu.classList.remove('top-[-250px]');
+        // sideMenu.classList.add('top-0');+
+};
+
+
+function OutMainNav(){
 top.value = '-top-[170px]';
+        // sideMenu.classList.remove('top-0');
+        // sideMenu.classList.add('top-[-250px]');
+};
+
+function OutMainNav2(){
+top2.value = '-top-[170px]';
+        // sideMenu.classList.remove('top-0');
+        // sideMenu.classList.add('top-[-250px]');
+};
+
+function OutMainNav3(){
+top3.value = '-top-[170px]';
+        // sideMenu.classList.remove('top-0');
+        // sideMenu.classList.add('top-[-250px]');
+};
+
+function OutMainNav4(){
+top4.value = '-top-[170px]';
+        // sideMenu.classList.remove('top-0');
+        // sideMenu.classList.add('top-[-250px]');
+};
+
+function OutMainNav5(){
+top5.value = '-top-[170px]';
         // sideMenu.classList.remove('top-0');
         // sideMenu.classList.add('top-[-250px]');
 };

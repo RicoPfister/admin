5caf45a974ddac733b6e8cfbf572c6779fc0542b
Rico Pfister <contact.rico.pfister@gmail.com>
2022-07-21 12:31:07 +0200

    Add Tailwind menu arrow Dynamic variable seems not possible in Javascript (also no variable names out of function variables)

--- resources/js/Module/Website/Components/WebsiteNav.vue
+++ resources/js/Module/Website/Components/WebsiteNav.vue
@@ -1,60 +1,51 @@
 <template>
 <div class="fixed flex justify-center w-full gap-2">
 
-    <div :class="top" class="relative gap-2 duration-500 border-black">
-        <div class="" @mouseover='OverMainNav' @mouseout='OutMainNav'>
-            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Curriculum<br>Hobbies<br>Social Engagements</div>
-            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">About Me</div>
+    <div :class="top1" class="relative gap-2 duration-500 border-black">
+        <div class="" @mouseover='OverMainNav1' @mouseout='OutMainNav1'>
+            <div class="w-52 h-[145px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">About Me<br>Work Experience<br>Skills<br>Hobbies<br>Social Engagements</div>
+            <div class="w-52 flex border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 justify-center">
+                About Me&nbsp;
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 duration-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path class="duration-700" stroke-linecap="round" stroke-linejoin="round" :d="menuArror1" d="M19 9l-7 7-7-7" />
+                </svg>
+            </div>
         </div>
     </div>
 
     <div :class="top2" class="relative gap-2 duration-500 border-black">
-        <div class="" @mouseover='OverMainNav2' @mouseout='OutMainNav2'>
-            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Curriculum2<br>Hobbies<br>Social Engagements</div>
-            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">About Me</div>
+        <div>
+            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3"></div>
+            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">Rico Database</div>
         </div>
     </div>
 
     <div :class="top3" class="relative gap-2 duration-500 border-black">
-        <div class="" @mouseover='OverMainNav3' @mouseout='OutMainNav3'>
-            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Curriculum2<br>Hobbies<br>Social Engagements</div>
-            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">About Me</div>
+        <div>
+            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3"></div>
+            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">Rico AI</div>
         </div>
     </div>
 
     <div :class="top4" class="relative gap-2 duration-500 border-black">
         <div class="" @mouseover='OverMainNav4' @mouseout='OutMainNav4'>
-            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Curriculum2<br>Hobbies<br>Social Engagements</div>
-            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">About Me</div>
+            <div class="w-52 h-[95px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Adventure Game<br>Cartoons<br>Archive</div>
+            <div class="w-52 flex border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 justify-center">
+                Other Projects&nbsp;
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 duration-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path class="duration-700" stroke-linecap="round" stroke-linejoin="round" :d="menuArror2" d="M19 9l-7 7-7-7" />
+                </svg>
+            </div>
         </div>
     </div>
 
     <div :class="top5" class="relative gap-2 duration-500 border-black">
-        <div class="" @mouseover='OverMainNav5' @mouseout='OutMainNav5'>
-            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Curriculum2<br>Hobbies<br>Social Engagements</div>
-            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">About Me</div>
+        <div>
+            <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3"></div>
+            <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 justify-center">Contact</div>
         </div>
     </div>
 </div>
-
-<!-- <div :class="top" class="w-full flex justify-center fixed gap-2 duration-500">
-    <div class="">
-        <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3"></div>
-        <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">Rico AI</div>
-    </div>
-</div> -->
-    <!-- <div class="">
-        <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3"></div>
-        <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">Rico AI</div>
-    </div>
-    <div class="">
-        <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3">Rico Database<br>Adventure Game<br>Archive</div>
-        <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">Other Projects</div>
-    </div>
-    <div class="">
-        <div class="w-52 h-[170px] border-green-100 border-l-4 border-r-4 pl-5 bg-green-100 pt-3"></div>
-        <div class="w-52 border-green-200 border-l-4 border-r-4 border-b-4 rounded-b-3xl text-center bg-green-200 grid content-end">Contact</div>
-    </div> -->
 
 </template>
 
@@ -70,99 +61,33 @@ let sideMenu = document.getElementById('main-menu');
 // Set up an event handler. Notice that we don't use "on" in front
 // of the event name when doing it this way.
 
-let top = ref('top-0');
-let top2 = ref('top-0');
-let top3 = ref('top-0');
-let top4 = ref('top-0');
-let top5 = ref('top-0');
+let top1 = ref('-top-[145px]');
+let top2 = ref('-top-[170px]');
+let top3 = ref('-top-[170px]');
+let top4 = ref('-top-[95px]');
+let top5 = ref('-top-[170px]');
+let menuArror1 = ref('M19 9l-7 7-7-7');
+let menuArror2 = ref('M19 9l-7 7-7-7');
 
-function OverMainNav(){
-top.value = 'top-0';
-//        top = '-top-36';
-//    console.log(top);
-//    alert(top);
-
-
-        // sideMenu.classList.remove('top-[-250px]');
-        // sideMenu.classList.add('top-0');+
-};
-
-function OverMainNav2(){
-top2.value = 'top-0';
-//        top = '-top-36';
-//    console.log(top);
-//    alert(top);
-
-
-        // sideMenu.classList.remove('top-[-250px]');
-        // sideMenu.classList.add('top-0');+
-};
-
-function OverMainNav3(){
-top3.value = 'top-0';
-//        top = '-top-36';
-//    console.log(top);
-//    alert(top);
-
-
-        // sideMenu.classList.remove('top-[-250px]');
-        // sideMenu.classList.add('top-0');+
+function OverMainNav1(){
+top1.value = 'top-0';
+menuArror1.value = 'M5 15l7-7 7 7'
 };
 
 function OverMainNav4(){
 top4.value = 'top-0';
-//        top = '-top-36';
-//    console.log(top);
-//    alert(top);
-
-
-        // sideMenu.classList.remove('top-[-250px]');
-        // sideMenu.classList.add('top-0');+
+menuArror2.value = 'M5 15l7-7 7 7'
 };
 
-function OverMainNav5(){
-top5.value = 'top-0';
-//        top = '-top-36';
-//    console.log(top);
-//    alert(top);
-
-
-        // sideMenu.classList.remove('top-[-250px]');
-        // sideMenu.classList.add('top-0');+
-};
-
-
-function OutMainNav(){
-top.value = '-top-[170px]';
-        // sideMenu.classList.remove('top-0');
-        // sideMenu.classList.add('top-[-250px]');
-};
-
-function OutMainNav2(){
-top2.value = '-top-[170px]';
-        // sideMenu.classList.remove('top-0');
-        // sideMenu.classList.add('top-[-250px]');
-};
-
-function OutMainNav3(){
-top3.value = '-top-[170px]';
-        // sideMenu.classList.remove('top-0');
-        // sideMenu.classList.add('top-[-250px]');
+function OutMainNav1(){
+top1.value = '-top-[145px]';
+menuArror1.value = 'M19 9l-7 7-7-7'
 };
 
 function OutMainNav4(){
-top4.value = '-top-[170px]';
-        // sideMenu.classList.remove('top-0');
-        // sideMenu.classList.add('top-[-250px]');
+top4.value = '-top-[95px]';
+menuArror2.value = 'M19 9l-7 7-7-7'
 };
-
-function OutMainNav5(){
-top5.value = '-top-[170px]';
-        // sideMenu.classList.remove('top-0');
-        // sideMenu.classList.add('top-[-250px]');
-};
-
-// const top = '-top-36';
 
 </script>
 
